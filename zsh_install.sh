2ef5b21314d16f211f6bea9bb5ad6fa9ef2c9389
Xi <jungheilai@qq.com>
2021-04-07 16:07:53 +0800

    fix

--- zsh_install.sh
+++ zsh_install.sh
@@ -10,6 +10,7 @@ if [ $(uname) == "Linux" ]; then
     if cat /etc/*release | grep ^NAME | grep Ubuntu 1>/dev/null 2>&1; then
         check_sudo "Ubuntu"
         sudo apt install -y git curl zsh tmux
+        sudo chsh -s /bin/zsh
     else
         echo "[ INSTALL VIM ] OS not support, exiting installation!"
         exit 3
@@ -22,12 +23,15 @@ else
     exit 3
 fi
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && \
 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting && \
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions || \
+exit 1
 
-cp ./zshrc $HOME/.zshrc
+sed -i "s/ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"agnoster\"/g" $HOME/.zshrc && \
+sed -i "s/# DISABLE_MAGIC_FUNCTIONS=\"true\"/DISABLE_MAGIC_FUNCTIONS=\"true\"/g" $HOME/.zshrc && \
+sed -i "s/plugins=(git)/plugins=(git z extract sudo command-not-found safe-paste tmux history zsh-syntax-highlighting zsh-autosuggestions)/g" $HOME/.zshrc || \
+exit 2
 
-sudo chsh -s /bin/zsh
 
