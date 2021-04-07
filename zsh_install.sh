c33a2c19a21219a9903a1311c4a7e748289dc2b3
Xi <jungheilai@qq.com>
2021-04-07 17:16:04 +0800

    fix

--- zsh_install.sh
+++ zsh_install.sh
@@ -23,15 +23,25 @@ else
     exit 3
 fi
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && \
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | grep -v exec)" && \
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting && \
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions || \
 exit 1
 
-sed -i "s/ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"agnoster\"/g" $HOME/.zshrc && \
-sed -i "s/# DISABLE_MAGIC_FUNCTIONS=\"true\"/DISABLE_MAGIC_FUNCTIONS=\"true\"/g" $HOME/.zshrc && \
-sed -i "s/plugins=(git)/plugins=(git z extract sudo command-not-found safe-paste tmux history zsh-syntax-highlighting zsh-autosuggestions)/g" $HOME/.zshrc || \
-exit 2
+if [ $(uname) == "Linux" ]; then
+    sed -i "s/ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"agnoster\"/g" $HOME/.zshrc && \
+    sed -i "s/# DISABLE_MAGIC_FUNCTIONS=\"true\"/DISABLE_MAGIC_FUNCTIONS=\"true\"/g" $HOME/.zshrc && \
+    sed -i "s/plugins=(git)/plugins=(git z extract sudo command-not-found safe-paste tmux history zsh-syntax-highlighting zsh-autosuggestions)/g" $HOME/.zshrc || \
+    exit 2
+elif [ $(uname) == "Darwin" ]; then
+    sed -i "" "s/ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"agnoster\"/g" $HOME/.zshrc && \
+    sed -i "" "s/# DISABLE_MAGIC_FUNCTIONS=\"true\"/DISABLE_MAGIC_FUNCTIONS=\"true\"/g" $HOME/.zshrc && \
+    sed -i "" "s/plugins=(git)/plugins=(git z extract sudo command-not-found safe-paste tmux history zsh-syntax-highlighting zsh-autosuggestions)/g" $HOME/.zshrc || \
+    exit 2
+else
+    echo "[ INSTALL VIM ] Unknown OS $(uname), exiting installation!"
+    exit 3
+fi
 
-
+echo -e "\033[32mFinish!\033[0m"
